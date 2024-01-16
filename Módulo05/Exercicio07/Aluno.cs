05dcfd30b2b301eb032e9dcf633d4237a16724cf
Lucas Pereira de Andrade <134231925+LucasAndrade1605@users.noreply.github.com>
2024-01-16 01:18:57 -0400

    Add files via upload

---
+++ Módulo05/Exercicio07/Aluno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio07
+{
+    public class Aluno
+    {
+        public string?[] Nome = new string[10];
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i >= 0 && i < Nome.Length) return Nome[i];
+                return $"Não há a posição {i}\n";
+            }
+            set 
+            {
+                if (i >= 0 && i < Nome.Length) Nome[i] = value;
+                else Console.WriteLine($"Não há a posição {i}\n");
+            }
+        }
+    }
+}
