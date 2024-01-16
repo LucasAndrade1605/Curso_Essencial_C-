bdf68ad626d6b965b13d2d6ae6481c2a4475ed4b
Lucas Pereira de Andrade <134231925+LucasAndrade1605@users.noreply.github.com>
2024-01-16 01:19:20 -0400

    Add files via upload

---
+++ Módulo05/Exercicio_ListT/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_ListT
+{
+    public class Aluno
+    {
+        public string Nome { get; set; } = string.Empty;
+        public Double Nota { get; set; }
+    }
+
+}
