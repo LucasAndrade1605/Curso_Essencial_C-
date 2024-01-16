8828fa7d2a85214f6da8769bf66508ee520afdb8
Lucas Pereira de Andrade <134231925+LucasAndrade1605@users.noreply.github.com>
2024-01-16 01:18:11 -0400

    Add files via upload

---
+++ Módulo05/Exercicio04/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio04
+{
+    internal class Pessoa
+    {
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {this.Nome}\nIdade: {this.Idade}\n");
+        }
+    }
+}
